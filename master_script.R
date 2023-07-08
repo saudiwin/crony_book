166d706f32c1006b73649b144b5b2f241bcb6b49
Robert Kubinec <bobkubinec@gmail.com>
2023-07-08 13:47:40 -0400

    more packrat

--- master_script.R
+++ master_script.R
@@ -8,3 +8,5 @@ source("rscripts/04a-Case-Study-Egypt.R")
 source("rscripts/05a-Case-Study-Tunisia.R")
 source("rscripts/06-Quantitative-Surveys.R")
 source("rscripts/07-Quantitative-Surveys-Other.R")
+
+rmarkdown::render("README.Rmd")
